3f12180822dc1feb321d6f81c032e74e5e9e14b2
Adam Hake <adam.hake@fostermade.co>
2025-10-16 14:57:05 -0400

    Use Optimized fonts

--- vite.config.ts
+++ vite.config.ts
@@ -5,22 +5,16 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
+import webfontDownload from "vite-plugin-webfont-dl";
 
 const config = defineConfig({
   plugins: [
-    devtools({
-      eventBusConfig: {
-        debug: false,
-      },
-      enhancedLogs: {
-        enabled: true,
-      },
-    }),
     viteTsConfigPaths({
       projects: ["./tsconfig.json"],
     }),
     tanstackStart({ sitemap: { enabled: true } }),
     netlify(),
+    webfontDownload(),
     tailwindcss(),
     viteReact(),
   ],
